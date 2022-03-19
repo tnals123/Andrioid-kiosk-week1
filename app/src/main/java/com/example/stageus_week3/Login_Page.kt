bd843be7400d90cb09bb59f8ceb1b52e5a558cd1
tnals123 <soo8608@naver.com>
2022-03-19 22:05:15 +0900

    update my practice

--- app/src/main/java/com/example/stageus_week3/Login_Page.kt
+++ app/src/main/java/com/example/stageus_week3/Login_Page.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import retrofit2.*
 
 class Login_Page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,36 +15,38 @@ class Login_Page : AppCompatActivity() {
         setContentView(R.layout.loginlayout)
 
         var loginbutton = findViewById<Button>(R.id.login)
-
-        var db = DataBase(this,"stageus.db",null,1) // 데이터베이스 클래스 객체
-        var readabledb = db.readableDatabase // 데이터베이스 객체를 읽기 가능한 상황으로로
-        var writeabledb = db.writableDatabase // 데이터베이스 객체를 쓰기 가능한 상황으로
-        var datacontrol = DataBase_Control()
-
-        var id = findViewById<EditText>(R.id.idedit).text.toString()
-        var pw = findViewById<EditText>(R.id.pwedit).text.toString()
-
-        var asdf = datacontrol.readaccount(readabledb,id,pw)
-
-        if(asdf.size == 0){
-            loginbutton.setOnClickListener() {
-                Log.d("asdfasdfasdfasdfasdf","asdfasdfasdfadsfasdfadf")
-            }
-
-        }
-
-        else {
-            loginbutton.setOnClickListener() {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-            }
-        }
-
         var signupbutton = findViewById<Button>(R.id.resister)
-        signupbutton.setOnClickListener(){
-            datacontrol.createaccount(writeabledb,findViewById<EditText>(R.id.idedit).text.toString(),
-                                        findViewById<EditText>(R.id.pwedit).text.toString())
+
+        loginbutton.setOnClickListener() {
+
+            var id = findViewById<EditText>(R.id.idedit).text.toString()
+            var pw = findViewById<EditText>(R.id.pwedit).text.toString()
+
+            //retrofit 실습 내용
+            loginbutton.setOnClickListener() {}
+            var retrofit = RetrofitClient.initRetrofit()
+
+            //api 와 통신!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            //enqueue 는 callback 함수임 비동기를 처리해주는 함수
+            var requestloginapi = retrofit.create(RetrofitClient.loginApi::class.java)
+            requestloginapi.getLogin(id, pw).enqueue(object : Callback<RetrofitClient.logindata> {
+                override fun onFailure(call: Call<RetrofitClient.logindata>, t: Throwable) {
+                }
+
+                override fun onResponse(
+                    call: Call<RetrofitClient.logindata>,
+                    response: Response<RetrofitClient.logindata>
+                ) {
+                    Log.d("result", response.body()!!.message)
+                    Log.d("result", response.body()!!.success.toString())
+                    var intent = Intent(this@Login_Page,MainActivity :: class.java)
+                    startActivity(intent)
+
+                }
+
+            })
         }
+
     }
 }
 
