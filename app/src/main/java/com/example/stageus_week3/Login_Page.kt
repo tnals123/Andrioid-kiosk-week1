b45afe030d813fb468b97cf71923e0f9545e801b
tnals123 <soo8608@naver.com>
2022-04-03 19:44:01 +0900

    update my practice

--- app/src/main/java/com/example/stageus_week3/Login_Page.kt
+++ app/src/main/java/com/example/stageus_week3/Login_Page.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.text.Html
 import android.util.Log
+import android.webkit.WebView
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -14,43 +15,10 @@ class Login_Page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.loginlayout)
-
-        var loginbutton = findViewById<Button>(R.id.login)
-        var signupbutton = findViewById<Button>(R.id.resister)
-
-
-
-        loginbutton.setOnClickListener() {
-
-            var id = findViewById<EditText>(R.id.idedit).text.toString()
-            var pw = findViewById<EditText>(R.id.pwedit).text.toString()
-
-            loginbutton.text = Html.fromHtml("<h2>Title</h2><br><p>"+pw+"</p>", Html.FROM_HTML_MODE_COMPACT)
-            Log.d("Asdfasdfasdfasdf",loginbutton.text.toString())
-            //retrofit 실습 내용
-            loginbutton.setOnClickListener() {}
-            var retrofit = RetrofitClient.initRetrofit()
-
-            //api 와 통신!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            //enqueue 는 callback 함수임 비동기를 처리해주는 함수
-            var requestloginapi = retrofit.create(RetrofitClient.loginApi::class.java)
-            requestloginapi.getLogin(id, pw).enqueue(object : Callback<RetrofitClient.logindata> {
-                override fun onFailure(call: Call<RetrofitClient.logindata>, t: Throwable) {
-                }
-
-                override fun onResponse(
-                    call: Call<RetrofitClient.logindata>,
-                    response: Response<RetrofitClient.logindata>
-                ) {
-                    Log.d("result", response.body()!!.message)
-                    Log.d("result", response.body()!!.success.toString())
-                    var intent = Intent(this@Login_Page,MainActivity :: class.java)
-                    startActivity(intent)
-
-                }
-
-            })
-        }
+        var webview = findViewById<WebView>(R.id.webview)
+        webview.settings.javaScriptEnabled = true
+        var weburl : String = "file:///android_asset/asdfasdf.html"
+        webview.loadUrl(weburl)
 
     }
 }
